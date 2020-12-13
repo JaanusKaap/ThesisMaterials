4b57d99773827fca87180781e73a32c03eb14000
Jaanus Kääp <jaanus.kaap@gmail.com>
2020-12-13 14:08:58 +0200

    Logging to filesystem works

--- DriversTools/SharedKernel/VMBusIntercept.cpp
+++ DriversTools/SharedKernel/VMBusIntercept.cpp
@@ -35,9 +35,6 @@ bool VMBusIntercept::hook(VMBusInteceptConf* conf)
 
 	hookedChannel = addr;
 	count = conf->count;
-	DbgPrint("conf->logToFile = 0x%X\n", conf->logToFile);
-	DbgPrint("conf->logToDebug = 0x%X\n", conf->logToDebug);
-	DbgPrint("conf->logToDebugAndBreak = 0x%X\n", conf->logToDebugAndBreak);
 	logToFile = (conf->logToFile & 1) > 0;
 	logToDebug = (conf->logToDebug & 1) > 0;
 	logToDebugAndBreak = (conf->logToDebugAndBreak & 1) > 0;
@@ -107,7 +104,48 @@ void VMBusIntercept::hookHandler(void* channel, void* packet, void* buffer, UINT
 		if(logToDebugAndBreak)
 			DbgBreakPoint();
 	}
+	if (count && logToFile)
+	{
+		PVMBUS_CHANNEL_RECORD_DATA data = (PVMBUS_CHANNEL_RECORD_DATA)ExAllocatePoolWithTag(NonPagedPoolNx, sizeof(VMBUS_CHANNEL_RECORD_DATA), 'VMbs');
+		data->WorkItem = (PWORK_QUEUE_ITEM)ExAllocatePoolWithTag(NonPagedPoolNx, sizeof(WORK_QUEUE_ITEM), 0x76687668);
+		data->buffer = ExAllocatePoolWithTag(NonPagedPoolNx, bufferLen, 'VMbs');
+		data->bufferLen = bufferLen;
+		memcpy(data->buffer, buffer, bufferLen);
+		data->external = NULL;
+		data->externalLen = 0;
+		if (flags & 0x1)
+		{
+			PMDL mdl = NULL;
+			if (VmbChannelPacketGetExternalData(packet, 0, &mdl) == STATUS_SUCCESS)
+			{
+				data->externalLen = MmGetMdlByteCount(mdl);
+				data->external = ExAllocatePoolWithTag(NonPagedPoolNx, data->externalLen, 'VMbs');
+				memcpy(data->external, MmGetSystemAddressForMdlSafe(mdl, NormalPagePriority), data->externalLen);
+			}
+		}
+		ExInitializeWorkItem(data->WorkItem, fileLogRoutine, data);
+		ExQueueWorkItem(data->WorkItem, DelayedWorkQueue);
+	}
 	if (count)
 		count--;
 	return originalHandler(channel, packet, buffer, bufferLen, flags);
+}
+
+void VMBusIntercept::fileLogRoutine(PVOID Parameter)
+{
+	PVMBUS_CHANNEL_RECORD_DATA data = (PVMBUS_CHANNEL_RECORD_DATA)Parameter;
+	if (logHandle)
+	{
+		IO_STATUS_BLOCK ioStatusBlock;
+		ZwWriteFile(logHandle, NULL, NULL, NULL, &ioStatusBlock, &(data->bufferLen), sizeof(data->bufferLen), NULL, NULL);
+		ZwWriteFile(logHandle, NULL, NULL, NULL, &ioStatusBlock, data->buffer, data->bufferLen, NULL, NULL);
+		ZwWriteFile(logHandle, NULL, NULL, NULL, &ioStatusBlock, &(data->externalLen), sizeof(data->externalLen), NULL, NULL);
+		if(data->externalLen)
+			ZwWriteFile(logHandle, NULL, NULL, NULL, &ioStatusBlock, data->external, data->externalLen, NULL, NULL);
+		ExFreePool(data->WorkItem);
+		ExFreePool(data->buffer);
+		if (data->externalLen)
+			ExFreePool(data->external);
+		ExFreePool(data);
+	}
 }
--- DriversTools/SharedKernel/VMBusIntercept.h
+++ DriversTools/SharedKernel/VMBusIntercept.h
@@ -6,6 +6,13 @@
 typedef void (*ChannelHandlerFunction)(void*, void*, void*, UINT32, UINT32);
 typedef NTSTATUS EVT_VMB_PACKET_GET_EXTERNAL_DATA(VOID*, UINT32, PMDL*);
 
+typedef struct VMBUS_CHANNEL_RECORD_DATA
+{
+	PWORK_QUEUE_ITEM WorkItem;
+	PVOID buffer, external;
+	UINT32 bufferLen, externalLen;
+}VMBUS_CHANNEL_RECORD_DATA, * PVMBUS_CHANNEL_RECORD_DATA;
+
 class VMBusIntercept : public VMBusChannel
 {
 protected:
@@ -18,6 +25,7 @@ protected:
 	static ChannelHandlerFunction originalHandler;
 
 	static void hookHandler(void* channel, void* packet, void* buffer, UINT32 bufferLen, UINT32 flags);
+	static void fileLogRoutine(PVOID Parameter);
 
 	void setInt64ToChannel(void* channel, UINT32 offset, UINT64 value);
 
